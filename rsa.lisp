dab9aa752a78810084369340594dfe73a8e75a57
Laurent Cimon <laurent@nilio.ca>
2020-11-16 21:47:36 -0500

    Cleanup the accessibility functions

--- rsa.lisp
+++ rsa.lisp
@@ -6,14 +6,20 @@
 	  nil
 	  (cons (car pair) (cadr pair)))))
 
-(defun cypher (e n M)
+(defun cypher (M n e)
   (modular-exponent M e n))
 
-(defun decypher (e n C)
+(defun get-d (e prime-pair)
+  (modular-inverse
+   e
+   (* (- (car prime-pair) 1)
+      (- (cdr prime-pair) 1))))
+
+(defun crack (C n e)
   (let* ((prime-pair (get-prime-pair n))
-	 (d
-	  (modular-inverse
-	   e
-	   (* (- (car prime-pair) 1)
-	      (- (cdr prime-pair) 1)))))
+	 (d (get-d e prime-pair)))
+    (modular-exponent C d n)))
+
+(defun decypher (C n e p q)
+  (let ((d (get-d e (cons p q))))
     (modular-exponent C d n)))
