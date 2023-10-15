24be35381b2aded3e55df70634850b7d3a930021
Laurent Cimon <laurent@nilio.ca>
2023-10-15 16:17:54 -0400

    Loi de Weibull et loi Normale

--- probabilite-continue.lisp
+++ probabilite-continue.lisp
@@ -8,15 +8,23 @@
 	(y2 (funcall f x2)
 	    (funcall f x2))
 	(h (/ (+ y1 y2) 2) (/ (+ y1 y2) 2))
-	(sum (* h dx) (+ sum (* h dx))))
-       ((>= x2 b) sum)))
+	(somme (* h dx) (+ somme (* h dx))))
+       ((>= x2 b) somme)))
 
 (defun approximation-integrale-infinie (f a dx)
   (let ((x-max
 	   (do* ((x a (+ x dx))
 		 (y (funcall f x) (funcall f x)))
-		((<= y 0.0001) x))))
+		((and (<= y 0.0001) (/= (round x) 0))
+		 x))))
     (approximation-integrale f a x-max dx)))
+
+(defun fonction-gamma (alpha)
+  (if (integerp alpha);(= alpha (round alpha))
+      (factorielle (- alpha 1))
+      (approximation-integrale-infinie
+       (lambda (x) (* (expt x (- alpha 1)) (exp (- x))))
+       0 0.1)))
 
 (defun Uniforme (alpha beta)
   ;; La loi uniforme a une fonction de densité constante
@@ -69,39 +77,108 @@
   ;; selon le taux donné. Cette fonction est écrite pour une valeur entière
   ;; de r.
 
-  (let* ((fonction-gamma
-	   (lambda (alpha) (factorielle (- alpha 1))))
-	 (moyenne (/ r taux))
+  (let* ((moyenne (/ r taux))
 	 (variance (/ r (expt taux 2)))
-	 (ecart-type (sqrt variance)))
+	 (ecart-type (sqrt variance))
+	 (densité
+	   (lambda (x)
+	     (*
+	      (/ taux (fonction-gamma r))
+	      (expt (* taux x) (- r 1))
+	      (exp (* (- taux) x))))))
 
     (list
      ; Densité
      (lambda (x)
        (if (<= x 0)
 	   0
-	   (*
-	    (/ taux (funcall fonction-gamma r))
-	    (expt (* taux x) (- r 1))
-	    (exp (* (- taux) x)))))
+	   (funcall densité x)))
      ; Répartition
      (lambda (x)
        (if (<= x 0)
 	   0
-	   (let ((somme-intégrée
-		   (lambda (k)
-		     (/ (*
-			 (exp (* (- taux) x))
-			 (expt (* taux x) k))
-			(factorielle k)))))
-	   (- 1
-	      (do* ((k 0 (1+ k))
-		    (sum (funcall somme-intégrée k)
-			 (+ sum (funcall somme-intégrée k))))
-		   ((>= k (- r 1)) sum))))))
+	   (if (= (round r) r)
+	       (let ((somme-intégrée
+		       (lambda (k)
+			 (/ (*
+			     (exp (* (- taux) x))
+			     (expt (* taux x) k))
+			    (factorielle k)))))
+		 (- 1
+		    (do* ((k 0 (1+ k))
+			  (sum (funcall somme-intégrée k)
+			       (+ sum (funcall somme-intégrée k))))
+			 ((>= k (- r 1)) sum))))
+	       (approximation-integrale densité 0 x 0.001))))
      moyenne
      variance
      ecart-type)))
+
+(defun Weibull (gamma beta delta)
+  ;; Weibull est une exponentielle à trois paramètres. Elle demande
+  ;; le paramètre de forme beta qui controle la direction de la fonction,
+  ;; le paramètre de dispersion delta est proportionnel à la variance,
+  ;; le paramètre gamma détermine la position de la courbe
+ 
+  (let* ((moyenne (+ gamma
+		     (* delta
+			(fonction-gamma (+ 1 (/ 1 beta))))))
+	 (variance (*
+		    (expt delta 2)
+		    (-
+		     (fonction-gamma (+ 1 (/ 2 beta)))
+		     (sqrt (fonction-gamma (+ 1 (/ 1 beta)))))))
+	 (ecart-type (sqrt variance)))
+ 
+    (list
+     ; Densité
+     (lambda (x)
+       (if (< x gamma)
+	   0
+	   (*
+	    (/ beta delta)
+	    (expt (/ (- x gamma) delta) (- beta 1))
+	    (exp (- (expt (/ (- x gamma) delta) beta))))))
+     ; Répartition
+     (lambda (x)
+       (if (< x gamma)
+	   0
+	   (- 1 (exp (- (expt (/ (- x gamma) delta) beta))))))
+     moyenne
+     variance
+     ecart-type)))
+
+(defun Normale (mu sigma2)
+  ;; La loi normale prend sa moyenne et son écart type en argument,
+  ;; elle modélise une courbe qui suit ces propriétés et qui est définie
+  ;; sur tout le domaine de x, se rapprochant à zéro en s'éloignant de
+  ;; la moyenne.
+
+  (let* ((moyenne mu)
+	 (variance sigma2)
+	 (ecart-type (sqrt sigma2))
+	 (densite (lambda (x)
+		    (*
+		     (/ 1 (* ecart-type (sqrt (* 2 PI))))
+		     (exp (* (- (/ 1 2))
+			     (expt (/ (- x mu) ecart-type) 2)))))))
+
+	 (list
+	   ; Densité
+	   densite
+	   ; Répartition
+	   (lambda (x)
+	     (let ((limite (* 4 ecart-type)))
+	       (if (< x (- mu limite))
+		   0
+		   (approximation-integrale
+		    densite
+		    (- mu limite)
+		    (min (+ mu limite) x)
+		    (/ ecart-type 1000)))))
+	   moyenne
+	   variance
+	   ecart-type)))
 
 (defun repartition-continue (loi x)
   (funcall (cadr loi) x))
