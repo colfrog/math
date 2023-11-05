42c2fa2426645eed74bc939c62075b185d4855ac
Laurent Cimon <laurent@nilio.ca>
2023-11-05 15:35:00 -0500

    Make a cons-based binary tree

---
+++ btree.lisp
@@ -0,0 +1,20 @@
+(defun make-tree (value &optional (node1 nil) (node2 nil))
+  (cons
+   value
+   (cons node1 node2)))
+
+(defun insert-tree (tree value)
+  (if tree
+      (if (<= value (car tree))
+	  (make-tree (car tree)
+		     (insert-tree (cadr tree) value)
+		     (cddr tree))
+	  (make-tree (car tree)
+		     (cadr tree)
+		     (insert-tree (cddr tree) value)))
+      (make-tree value)))
+
+(defun build-tree (l &optional (tree nil))
+  (if (car l)
+      (build-tree (cdr l) (insert-tree tree (car l)))
+      tree))
